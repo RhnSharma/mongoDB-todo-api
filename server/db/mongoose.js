c2f8b508a1c83391e9258bad804e2af6bdb5a916
Rohan Sharma <rhnsharma5113@gmail.com>
2018-09-15 13:28:05 +0530

    made changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,10 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.URI);
+mongoose.connect(
+  process.env.URI,
+  {
+    useMongoClient: true
+  }
+);
 module.exports = { mongoose };
